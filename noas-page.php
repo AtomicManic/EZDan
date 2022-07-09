2c4dcea8eba971261fb48c61bc24bc51a37e3fdf
Suf <sufkarmon2@gmail.com>
2022-07-09 21:31:41 +0300

    layout of the page, add images

---
+++ noas-page.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EZDan</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!--Bootstrap css-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body class="noas-page">
+        <header class= "container-fluid">
+            <!-- <section class= "row top-header">
+                <nav>
+                    here goes the nav
+                </nav>
+                <a href="#" class = "EZDan-logo">logo</a>
+                <a href="#" class= main-user-img>AlonaIMage</a>
+            </section>
+            <section class= "row middle-header">
+
+            </section>
+            <section class= "row bottom-header">
+
+            </section> -->
+        </header>
+        <main>
+            <h3>My Destinations</h3>
+            <section class ="destinations containet-fluid">
+                <section class="destination row">
+                    <div class="col destination-details">
+                        <span>Home</span>
+                    </div>
+                    <div class="col destination-details">
+                        <span>Haroe'e 43 Ramat Gan</span>
+                    </div>
+                    <div class="col destination-details">
+                        <div class="row justify-content-center">
+                            <button type="button" class="btn btn-info col-6">Choose line</button>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col">
+                                <button type="button" class="btn btn-outline-secondary">Edit</button>
+                            </div>
+                            <div class="col">
+                                <button type="button" class="btn btn-outline-secondary">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="destination row">
+                    <div class="col destination-details">
+                        <span>from</span>
+                    </div>
+                    <div class="col destination-details">
+                        <span>to</span>
+                    </div>
+                    <div class="col destination-details">
+                        <span>actions</span>
+                    </div>
+                </section>
+                <section class="destination row">
+                    <div class="col destination-details">
+                        <span>from</span>
+                    </div>
+                    <div class="col destination-details">
+                        <span>to</span>
+                    </div>
+                    <div class="col destination-details">
+                        <span>actions</span>
+                    </div>
+                </section>
+            </section>
+
+            <div class="add-destination-btn">
+                <button type="button" class="btn btn-primary">Add destination</button>
+            </div>
+            
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
