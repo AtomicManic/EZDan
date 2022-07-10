e4a7dc1d751bed18cfc32bacf633c9245c77a89b
Adva <adva.apelboim@gmail.com>
2022-07-10 14:42:39 +0300

    almost-done

--- linesPage.php
+++ linesPage.php
@@ -1,7 +1,15 @@
+<?php
+
+    session_start();
+
+    echo 'id: ' . $_SESSION['id'] .', name: ' . $_SESSION['name'];
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Ride Page</title>
+        <title>Home Page</title>
         <!-- Meta -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -15,19 +23,19 @@
         <link rel="stylesheet" href="css/style.css">
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    </head>
-    <body class="ride-page">
+    </head> 
+    <body class="linesPageChild">
         <div class="container-fluid">
-            <header class="row top-header">
+        <header class="row top-header">
                 <div class="col-6 headerItem">
-                <a href="#" id="logo">
-                    <img src="images/EZDan-logo.png" alt="">
-                </a>
-                </div>
-                <div class="col-6 headerItem">
-                <a href="">
-                    <img src="" alt="avatar">
-                </a>
+                    <a href="#" id="logo">
+                        <img src="images/EZDan-logo.png" alt="">
+                    </a>
+                    </div>
+                    <div class="col-6 headerItem">
+                    <a href="">
+                        <img src="" alt="avatar">
+                    </a>
                 </div>
             </header>
             <div class="row menu">
@@ -45,7 +53,7 @@
                 </a>
             </div>
             <div class="row fromToInfo">
-                <section class="col-6 fromToItem">
+                <section class="col-4 fromToItem">
                     <span>Ride From:</span>
                     <span class="fromToText" id="departure">From json</span>
                 </section>
@@ -54,55 +62,39 @@
                     <span class="fromToText">From db</span>
                 </section>
             </div>
-            <div class="row lineInfo">
-                <section class="col-6 lineInfoItem">
-                    <span>Line:</span>
-                    <span class="lineInfoText" id="lineNumber">from json</span>
-                </section>
-                <section class="col-6 lineInfoItem">
-                    <span>Station:</span>
-                    <span class="lineInfoText" id="station">from json</span>
-                </section>
-            </div>
             <button type="button" class="btn btn-light changeBtn">Change</button>
-        <div class="container-fluid gradesContainer">
-            <div class="row">
-                <article id="noisGrade" class="gradeItem col-sm-6">
-                    <h5>from json</h5>
-                    <i></i>
-                    <span></span>
-                </article>
-                <article id="congestionGrade" class="gradeItem col-sm-6">
-                <h5>from json</h5>
-                    <i></i>
-                    <span></span>
-                </article>
-            </div>
-            <div class="row">
-                <article id="availableSeats" class="gradeItem col-sm-6">
-                    <h5>from json</h5>
-                    <i></i>
-                    <span></span>
-                </article>
-                <article id="wheelChairSeat" class="gradeItem col-sm-6">
-                <h5>from json</h5>
-                    <i></i>
-                    <span></span>
-                </article>
-            </div>
-            <div class="row totalScoreItem">
-                <span>Total Score:</span>
-                <span id="totalScore"></span>
-            </div>
         </div>
-        <div class="row selectRide">
-            <button type="submit" >Select
-            </button>
-        </div>
-        
-        </div>
-        
-
+        <article class="row lineInfo">
+            <div class="row walkAndLine">
+                <div class="col-5 walkAndLineItem">
+                    <span>100m</span>
+                    <i class="fa-solid fa-person-walking"></i>
+                    <i class="fa-solid fa-arrow-right-long"></i>
+                </div>
+                <div class="col-2 walkAndLineItem">
+                    <span>61</span>
+                </div>
+                <div class="col-5 walkAndLineItem">
+                    <i class="fa-solid fa-arrow-right-long"></i>
+                    <i class="fa-solid fa-person-walking"></i>
+                    <span>150m</span>
+                </div>
+            </div>
+            <div class="row stationAndGrade">
+                <div class="col-sm-4 stationAndGradeItam">
+                    <span>From: <span class="bold">Bialik/Habanim</span></span>
+                    <span>To: <span class="bold">Haroe'e/Tuval</span> </span>
+                </div>
+                <div class="col-sm-4 stationAndGradeItam">
+                    <p>Grade</p>
+                    <p class="bold">8.5</p>
+                </div>
+                <div class="col-sm-4 stationAndGradeItam">
+                    <button type="submit" class="btn selectRideBtn">Ride
+                    </button>
+                </div>
+            </div>
+        </article>
         <script src="scripts/getRide.js"></script>
     </body>
 </html>
