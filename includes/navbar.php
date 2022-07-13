873a244e91555b18b0d0725382c8d3c64aaa2a7c
Suf <sufkarmon2@gmail.com>
2022-07-13 21:36:41 +0300

    finish navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light navbar-expand-lg">
-  <div class="container-fluid">
+  <div class="container-fluid wrapper">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" 
              aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
              <span class="navbar-toggler-icon"></span>
